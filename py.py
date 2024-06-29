e5d24a81147a2a2886dfcbd33249eb77333b9fe6
KHAVIN S <147326178+khavindev@users.noreply.github.com>
2024-06-29 09:43:29 +0530

    Update py.py

--- py.py
+++ py.py
@@ -41,7 +41,7 @@ with st.sidebar:
         st.write("Copyright @ **TextFusion**")
         st.write("[My LinkedIn](https://www.linkedin.com/in/s-khavin73/)")
         st.write("[Github](https://github.com/khavindev)")
-        st.write("[Instagram](https://instagram.com/curiosity.ai_)")
+        st.write("[Instagram](https://instagram.com/textfusion.ai)")
         st.write("if you have any issues ping me on linkedin or instagram")
         st.write("[Powered by gemini-1.5]")
 
