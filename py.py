27ca1988a00e6412a5d82109887c453ba8dc6fea
KHAVIN S <147326178+khavindev@users.noreply.github.com>
2024-07-01 19:34:49 +0530

    Update py.py

--- py.py
+++ py.py
@@ -7,7 +7,7 @@ genai.configure(api_key=GOOGLEAI_API_KEY)
 # Model configuration
 generation_config = {
   "temperature": 1,
-  "top_p": 0.95,
+  "top_p": 0.95, #hi
   "top_k": 64,
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
